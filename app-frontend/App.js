4638d2f9cf7ecfaf559bf45150932aa5a2280c59
AswinSTS <aswin.s@deltasoft.in>
2023-12-21 18:44:03 +0530

    small change made

--- app-frontend/App.js
+++ app-frontend/App.js
@@ -20,8 +20,7 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      {/* <Stack.Navigator initialRouteName="Login"></Stack.Navigator> */}
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen
           name="Crowdly"
           component={HomeScreen}
