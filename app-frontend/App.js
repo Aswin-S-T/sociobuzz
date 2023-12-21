1622ab9773fdf61c1603e8f291a9695113b96f26
AswinSTS <aswin.s@deltasoft.in>
2023-12-21 09:37:19 +0530

    search bar added

--- app-frontend/App.js
+++ app-frontend/App.js
@@ -1,23 +1,30 @@
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import LoginScreen from "./src/Screens/LoginScreen";
 import HomeScreen from "./src/Screens/HomeScreen";
 import { createStackNavigator } from "@react-navigation/stack";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
 export default function App() {
+  const [isSearchVisible, setIsSearchVisible] = useState(false);
+
+  const toggleSearchVisibility = () => {
+    setIsSearchVisible((prev) => !prev);
+  };
+
   return (
     <NavigationContainer>
-      {/* <Stack.Navigator initialRouteName="Login"> */}
-        <Stack.Navigator>
+      <Stack.Navigator>
         <Stack.Screen
           name="Crowdly"
           component={HomeScreen}
-          options={{
+          options={({ navigation }) => ({
             headerTitle: "Crowdly",
             headerStyle: {
               backgroundColor: "#fff",
@@ -27,13 +34,32 @@ export default function App() {
               shadowRadius: 20,
               elevation: 5,
             },
-
             headerTitleStyle: {
               color: "#0E3D8B",
               fontWeight: "bold",
               fontFamily: "sans-serif",
             },
-          }}
+            headerRight: () => (
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                {isSearchVisible && (
+                  <TextInput
+                    style={styles.searchInput}
+                    placeholder="Search..."
+                    onChangeText={(text) => {
+                      // Handle search logic with the input text
+                    }}
+                  />
+                )}
+                <MaterialIcons
+                  name={isSearchVisible ? "close" : "search"}
+                  size={24}
+                  color="#0E3D8B"
+                  style={{ marginRight: 15 }}
+                  onPress={toggleSearchVisibility}
+                />
+              </View>
+            ),
+          })}
         />
         <Stack.Screen
           name="Login"
@@ -48,7 +74,6 @@ export default function App() {
               shadowRadius: 20,
               elevation: 5,
             },
-
             headerTitleStyle: {
               color: "#74DF00",
               fontWeight: "bold",
@@ -67,5 +92,16 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  searchInput: {
+    height: 30,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginRight: 10,
+    paddingLeft: 5,
+    width: "80%",
+    borderTopWidth:0,
+    borderLeftWidth:0,
+    borderRightWidth:0
   },
 });
