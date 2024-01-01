f4bfbf95d13b942f017b1bd445e273a34e26230c
AswinSTS <aswin.s@deltasoft.in>
2024-01-01 22:27:52 +0530

    smalll changed made

--- app-frontend/src/Components/Story.js
+++ app-frontend/src/Components/Story.js
@@ -1,33 +1,147 @@
-import React from "react";
-import { View, Text, FlatList, Image, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  StyleSheet,
+  Button,
+  ToastAndroid,
+  Modal,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
+import { firebase } from "../Config/firebase";
+import { v4 as uuidv4 } from "uuid";
+import { getRandomBytes } from "expo-random";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { BACKEND_URL } from "../Constants/Api";
 
 const Story = () => {
-  const stories = [
-    { id: "1", username: "John", imageUrl: "https://placekitten.com/60/60" },
-    { id: "2", username: "Jane", imageUrl: "https://placekitten.com/61/60" },
-    { id: "3", username: "Doe", imageUrl: "https://placekitten.com/62/60" },
-    { id: "4", username: "John", imageUrl: "https://placekitten.com/60/60" },
-    { id: "5", username: "Jane", imageUrl: "https://placekitten.com/61/60" },
-    { id: "6", username: "Doe", imageUrl: "https://placekitten.com/62/60" },
-  ];
+  const [uid, setUid] = useState("637360dbc8559f2ffa05acd5");
+  const [stories, setStories] = useState([]);
+  const [selectedStory, setSelectedStory] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
+
+  useEffect(() => {
+    const fetchStory = async () => {
+      const response = await fetch(`${BACKEND_URL}/api/v1/user/get-story`);
+      const data = await response?.json();
+      setStories(data?.data);
+    };
+    fetchStory();
+  }, []);
 
   const renderStoryItem = ({ item }) => (
-    <View style={styles.storyContainer}>
-      <Image source={{ uri: item.imageUrl }} style={styles.storyImage} />
+    <TouchableOpacity
+      style={styles.storyContainer}
+      onPress={() => {
+        setSelectedStory(item.story);
+        setModalVisible(true);
+      }}
+    >
+      <Image source={{ uri: item?.story }} style={styles.storyImage} />
       <Text style={styles.username}>{item.username}</Text>
-    </View>
+    </TouchableOpacity>
   );
+
+  const showToastWithGravity = () => {
+    ToastAndroid.showWithGravity(
+      "Story added",
+      ToastAndroid.SHORT,
+      ToastAndroid.CENTER
+    );
+  };
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      const blob = await new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+          resolve(xhr.response);
+        };
+        xhr.onerror = function () {
+          reject(new TypeError("Network request failed"));
+        };
+        xhr.responseType = "blob";
+        xhr.open("GET", result?.uri, true);
+        xhr.send(null);
+      });
+      const randomBytes = await getRandomBytes(16);
+      const imageName = `Pictures/Image_${uuidv4({
+        random: [...randomBytes],
+      })}`;
+
+      const ref = firebase.storage().ref().child(imageName);
+      const snapshot = await ref.put(blob);
+
+      const downloadURL = await snapshot.ref.getDownloadURL();
+
+      let data = {
+        userId: uid,
+        story: downloadURL,
+        username: "Aswin",
+      };
+      const response = await fetch(`${BACKEND_URL}/api/v1/user/upload-story`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      if (response && response.ok) {
+        showToastWithGravity();
+      }
+    }
+  };
 
   return (
     <View style={styles.container}>
+      <TouchableOpacity onPress={pickImage}>
+        <AntDesign
+          name="pluscircle"
+          size={62}
+          color="darkcyan"
+          style={{ top: 0, position: "relative" }}
+        />
+      </TouchableOpacity>
       <FlatList
         data={stories}
-        keyExtractor={(item) => item.id}
+        keyExtractor={(item) => item._id}
         renderItem={renderStoryItem}
         horizontal
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.storyList}
       />
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalContainer}>
+          {console.log("SELECTED STORY----------", selectedStory)}
+          <Image
+            source={{ uri: selectedStory }}
+            style={styles.selectedStoryImage}
+          />
+          <TouchableOpacity
+            onPress={() => setModalVisible(false)}
+            style={styles.closeButton}
+          >
+            <AntDesign name="closecircle" size={30} color="white" />
+          </TouchableOpacity>
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -36,6 +150,9 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#fff",
     paddingVertical: 10,
+    display: "flex",
+    flexDirection: "row",
+    margin: 4,
   },
   storyList: {
     paddingHorizontal: 10,
@@ -55,6 +172,11 @@ const styles = StyleSheet.create({
     marginTop: 5,
     fontFamily: "sans-serif",
     color: "#333",
+  },
+  selectedStoryImage: {
+    width: "100%",
+    height: "90%",
+    resizeMode: "cover",
   },
 });
 
